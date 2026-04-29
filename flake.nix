7c658f9dae419fe3068929255a4a1f99b449fb1c
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-04-29 10:25:38 -0400

    chore: release v0.1.1

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       bob,
     }:
     let
-      spinclassVersion = "0.1.0";
+      spinclassVersion = "0.1.1";
       # shortRev for clean builds, dirtyShortRev for dirty trees so devshell
       # builds visibly read `dirty-abcdef` instead of impersonating a release.
       spinclassCommit = self.shortRev or self.dirtyShortRev or "unknown";
