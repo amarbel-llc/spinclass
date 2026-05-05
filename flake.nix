8353ea6f8f39613aa68b7f9e9a4417d3cf5aa281
friedenberg <1baggzpr@starbrandshoes.com>
2026-05-05 10:03:09 -0400

    chore: release v0.1.7

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       bob,
     }:
     let
-      spinclassVersion = "0.1.6";
+      spinclassVersion = "0.1.7";
       # shortRev for clean builds, dirtyShortRev for dirty trees so devshell
       # builds visibly read `dirty-abcdef` instead of impersonating a release.
       spinclassCommit = self.shortRev or self.dirtyShortRev or "unknown";
