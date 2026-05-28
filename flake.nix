6c2d34b271a037a81459f6afb000b87dc3c8cc85
sfriedenberg-etsy <sfriedenberg@etsy.com>
2026-05-28 16:38:08 -0400

    chore: release v0.1.15

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
       bats,
     }:
     let
-      spinclassVersion = "0.1.14";
+      spinclassVersion = "0.1.15";
       # shortRev for clean builds, dirtyShortRev for dirty trees so devshell
       # builds visibly read `dirty-abcdef` instead of impersonating a release.
       spinclassCommit = self.shortRev or self.dirtyShortRev or "unknown";
